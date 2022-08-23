775c1032dd7ba248e7082dc05efb0e9918b78c2c
nazar_zavrin <nzavrin1404@gmail.com>
2022-08-23 17:27:42 +0300

    v0.1

--- script/script.js
+++ script/script.js
@@ -3,53 +3,51 @@
 let codeScreen = document.querySelector(".code-screen");
 let transparentInput = document.querySelector(".transparent-input");
 
-let keyWordsArray = ["let", "const"]
-let keyWordsRegexp = new RegExp("[>\\W]" + keyWordsArray.join("(?:&nbsp;| )|[>\\W]") + "(?:&nbsp;| )", "g");
+let keyWordsArray = ["let", "const", "function", "class", "new"];
+let keyWordsRegexp = new RegExp("(?<=\\W)" + keyWordsArray.join("(?=&nbsp;| )|(?<=\\W)") + "(?=&nbsp;| )", "gu");
 // &nbsp; - код HTML для отображения пробела
 console.log(keyWordsRegexp);
-// console.log("jj let a const b".match(keyWordsRegexp));
 
+// console.log("<>".match(/\W/g));
+// console.log("jj let a const b".match(keyWordsRegexp));
+// console.log("12hy".indexOf("h"));
 
 transparentInput.addEventListener("input", event => {
-    
-    let keyWords = transparentInput.innerHTML.match(keyWordsRegexp);
-    console.log("transparentInput.innerHTML\n"+transparentInput.innerHTML);
-    console.log("keyWords\n"+keyWords);
-    
 
-    let code = "";
-    let text = transparentInput.innerHTML.split(keyWordsRegexp);
+    
+    
+    // console.log("transparentInput.innerHTML\n"+transparentInput.innerHTML);
+    
+    let code = transparentInput.innerHTML;
+    let i = code.indexOf("<div>");
+    // console.log(i);
+    if (i < 0) {
+        code = "<div>" + code + "</div>";
+    } else if (i > 0){
+        code = "<div>" + code.substring(0, i) + "</div>" + code.slice(i);
+    }
+    // console.log(code);
+    // code = "";
+    // console.log("code\n"+code);
+    let keyWords = code.match(keyWordsRegexp);
+    console.log("keyWords\n",keyWords);
+    let text = code.split(keyWordsRegexp);
+    console.log("text\n",text);
+    
     if (keyWords) {
+        code = "";
         keyWords.forEach((keyWord, index) => {
-            // console.log(text[index], keyWord);
-            code += `<span class='text'>${text[index]}</span><span class='keyword'>${keyWord}</span>`;
+            // console.log("text\n" + text[index] + "\nkeyWord\n" + keyWord);
+
+            code += `${text[index]}<div class='keyword'>${keyWord}</div>`;
         })
         code += text.pop();
-        console.log("=====");
-    } else {
-        code = transparentInput.innerHTML;
-    }
-    // code
-
+        // console.log("=====");
+    } // else {
+    //     code = transparentInput.innerHTML;
+    // }
+    console.log(code);
     codeScreen.innerHTML = code;
+    console.log("=====");
 })
 
-// function replaceAll(string, substr, replacement){
-//     string.match(new Regexp(substr, "g"))
-// }
-
-/*
-codeScreen.addEventListener("keyup", event => {
-    console.log(codeScreen.innerHTML);
-    let words = codeScreen.textContent.split(/\s/);
-    let html = "";
-    words.forEach(word => {
-        if (word in keyWords) {
-            html += `<div class="keyword">${word}</div> `;
-        } else {
-            html += word + " ";
-        }
-    })
-    codeScreen.textContent = "";
-    codeScreen.insertAdjacentHTML("beforeend", html);
-})*/
